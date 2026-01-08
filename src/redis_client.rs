5d430d5791c3672df00d09039f70f017ea14b9ff
carl hedgren <carl.hedgren@nav.no>
2026-01-08 10:26:34 +0100

    spawn_blocking instead of just blockign
    We don't need to block the world here.
    (Surely the redis stuff has an ergonic async client somehwere??)

--- src/redis_client.rs
+++ src/redis_client.rs
@@ -1,7 +1,8 @@
 use crate::config::ValkeyConfig;
 use async_trait::async_trait;
-use redis::{Commands, Connection, RedisResult};
+use redis::{Commands, Connection, ErrorKind, RedisError, RedisResult};
 use std::collections::HashMap;
+use tokio::task;
 use tracing::error;
 
 #[async_trait]
@@ -11,14 +12,16 @@ pub trait ValkeyClient: Send {
 }
 
 pub struct ValkeyStore {
-    connection: Connection,
+    connection: Option<Connection>,
 }
 
 impl ValkeyStore {
     pub fn connect(config: &ValkeyConfig) -> Option<Self> {
         match redis::Client::open(config.uri.clone()) {
             Ok(client) => match client.get_connection() {
-                Ok(connection) => Some(Self { connection }),
+                Ok(connection) => Some(Self {
+                    connection: Some(connection),
+                }),
                 Err(err) => {
                     error!("Opening connection to Valkey failed: {err}");
                     None
@@ -35,11 +38,66 @@ impl ValkeyStore {
 #[async_trait]
 impl ValkeyClient for ValkeyStore {
     async fn get(&mut self, key: &str) -> RedisResult<Option<String>> {
-        self.connection.get(key)
+        let key = key.to_owned();
+        let mut conn = match self.connection.take() {
+            Some(c) => c,
+            None => {
+                return Err(RedisError::from((
+                    ErrorKind::IoError,
+                    "Valkey connection not available",
+                )))
+            }
+        };
+
+        let result = task::spawn_blocking(move || {
+            let res = conn.get(key);
+            (conn, res)
+        })
+        .await;
+
+        match result {
+            Ok((conn, res)) => {
+                self.connection = Some(conn);
+                res
+            }
+            Err(e) => Err(RedisError::from((
+                ErrorKind::IoError,
+                "spawn_blocking join error",
+                e.to_string(),
+            ))),
+        }
     }
 
     async fn set(&mut self, key: &str, value: &str) -> RedisResult<()> {
-        self.connection.set(key, value)
+        let key = key.to_owned();
+        let value = value.to_owned();
+        let mut conn = match self.connection.take() {
+            Some(c) => c,
+            None => {
+                return Err(RedisError::from((
+                    ErrorKind::IoError,
+                    "Valkey connection not available",
+                )))
+            }
+        };
+
+        let result = task::spawn_blocking(move || {
+            let res = conn.set(key, value);
+            (conn, res)
+        })
+        .await;
+
+        match result {
+            Ok((conn, res)) => {
+                self.connection = Some(conn);
+                res
+            }
+            Err(e) => Err(RedisError::from((
+                ErrorKind::IoError,
+                "spawn_blocking join error",
+                e.to_string(),
+            ))),
+        }
     }
 }
 
